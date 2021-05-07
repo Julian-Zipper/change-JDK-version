559e58148582430b85e1af1265e0a0d957b848db
Julian Zipper <julian_1992@live.nl>
2021-05-07 14:31:30 +0200

    Added elevation process to batch scripts
    Instead of using a 3rd party elevation script for admin privileges,
    each bash script contains a check for admin privileges. If they don't
    have those privileges, they will restart THEMSELVES, and this time
    WITH the actual admin privileges. Then they run the code for updating
    the jdk.
    
    Also changed some comments in the bash script.

--- change_java.sh
+++ change_java.sh
@@ -7,20 +7,17 @@ then
 	exit 1;
 fi
 
-# Third party script used to run .bat scripts with admin privileges
-elevate = /d/projects/scripts/elevate.bat;
-
 # If the java version is valid (an installed JDK):
 # - use a script to open an elevated powershell prompt to modify env variables
 # - call the corresponding batch script to update the Java env variables
 case $1 in
 	11 )
-		echo "Updating JAVA_HOME and PATH to jdk-$1";
-		$elevate ./jdk-11.bat;
+		echo "hello registry my old friend, i've come to update to java $1 again...";
+		./jdk-11.bat;
 		;;
 	16 )
-		echo "Updating JAVA_HOME and PATH to jdk-$1";
-		$elevate ./jdk-16.bat;
+		echo "hello registry my old friend, i've come to update to java $1 again...";
+		./jdk-16.bat;
 		;;
 	* )
 		echo "Not a valid Java version. Installed jdks are (11, 16)";
@@ -29,5 +26,7 @@ case $1 in
 esac
 
 # Closing statement
+echo "Batch script finished updating JAVA_HOME and PATH";
 echo "Succesfuly switched to jdk-$1";
 echo "Closing...";
+exit 0;
--- jdk-11.bat
+++ jdk-11.bat
@@ -1,4 +1,35 @@
-@Echo Off
+@ECHO OFF
+GOTO checkAdmin
 
-setx -m JAVA_HOME "D:\Program Files\Java\jdk11.0.11"
-setx -m PATH "%PATH%;%JAVA_HOME%\bin"
+:: Check if batch-file is run with admin privileges
+:checkAdmin
+	FSUTIL dirty query %systemdrive% >nul 2>&1
+	IF %errorLevel% == 1 (
+		GOTO isNotAdmin
+	) ELSE (
+		GOTO isAdmin
+	)
+
+:isNotAdmin
+	:: Create elevated process that runs this file again
+	SETLOCAL DisableDelayedExpansion
+	SET "batchPath=%~0"
+	SETLOCAL EnableDelayedExpansion
+
+	ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
+	ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+	"%temp%\OEgetPrivileges.vbs"
+	EXIT /B
+
+	GOTO EOF
+
+:isAdmin
+	CALL :codeToExecute
+
+	GOTO EOF
+	
+:codeToExecute
+	:: Actual code that needs executin' - pointing env vars to jdk-11
+	setx -m JAVA_HOME "D:\Program Files\Java\jdk11.0.11"
+	setx -m PATH "%PATH%;%JAVA_HOME%\bin"
+:EOF
